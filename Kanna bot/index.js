c22bcfc4afd881e572d43c21186a98af11583c73
aittegge <74157795+aittegge@users.noreply.github.com>
2020-11-14 20:06:18 +0100

    Add files via upload

---
+++ Kanna bot/index.js
@@ -0,0 +1,44 @@
+Discord = require ( 'discord.js' ) ,
+    client = new Discord.Client({
+        partials: ['MESSAGE', 'REACTION'],
+        fetchAllMembers: true
+    }),
+    config = require('./config.json'),
+    fs = require('fs')
+ 
+client.login(config.token)
+client.commands = new Discord.Collection()
+ 
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+ 
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+ 
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    if (command.guildOnly && !message.guild) return message.channel.send('Cette commande ne peut être utilisée que dans un serveur.')
+    command.run(message, args, client)
+})
+ 
+client.on('guildMemberAdd', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(`${member}`, new Discord.MessageEmbed()
+        .setDescription(`${member} a rejoint le serveur. Nous sommes désormais ${member.guild.memberCount} ! 🎉`)
+        .setColor('#00ff00'))
+    member.roles.add(config.greeting.role)
+})
+ 
+client.on('guildMemberRemove', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(new Discord.MessageEmbed()
+        .setDescription(`${member.user.tag} a quitté le serveur... 😢`)
+        .setColor('#ff0000'))
+})
